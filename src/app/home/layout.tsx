e783fec60c04ab5bb8dd2b82d3c1af3680fabf4b
Davi Castilho <davicastilho040@gmail.com>
2025-10-25 09:57:58 -0300

    fix: desativando funções inutilizadas

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -11,7 +11,7 @@ export default function HomeLayout({ children }: { children: React.ReactNode }) 
         <div className="navbar-logo">ClauseSpot</div>
         <div className="navbar-links">
           <Link href="/home" className={pathname === "/home" ? "active" : ""}>Home</Link>
-          <Link href="/home/buscar" className={pathname === "/home/buscar" ? "active" : ""}>Buscar</Link>
+          {/* <Link href="/home/buscar" className={pathname === "/home/buscar" ? "active" : ""}>Buscar</Link> */}
           <Link href="/home/uploadArquivos" className={pathname === "/home/uploadArquivos" ? "active" : ""}>Arquivos</Link>
           <Link href="/home/leis" className={pathname === "/home/leis" ? "active" : ""}>Leis</Link>
           <Link href="/home/gerenciaDeUsuario" className={pathname === "/home/gerenciaDeUsuario" ? "active" : ""}>Usuários</Link> 
