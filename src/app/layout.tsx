b1207645bf1cff0b4e098a18aa6b3dd9309efaea
Leonardo Hilario <leonardohilariordr@gmail.com>
2025-11-07 22:11:20 -0300

    feat: adicionei a importacao do globals.css no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"; // ✅ Importa o sistema global de toast
 
 const geistSans = Geist({
